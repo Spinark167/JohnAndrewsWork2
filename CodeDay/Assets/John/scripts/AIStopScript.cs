47dee4ffb53622d83c8682df8ed053be5fe024cb
Spinark167 <jhandrews1@gmail.com>
2015-02-15 00:04:48 -0500

    Grund is now actually done.
    Woot

---
+++ CodeDay/Assets/John/scripts/AIStopScript.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AIStopScript : MonoBehaviour {
+
+	public float MinDist;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GameObject guy = GameObject.Find ("Guy");
+		float guyX = guy.transform.position.x;
+		float guyZ = guy.transform.position.z;
+
+		float AIx = gameObject.transform.position.x;
+		float AIz = gameObject.transform.position.z;
+
+		float diffX = guyX - AIx;
+		float diffZ = guyZ - AIz;
+
+		float partDist = diffX * diffX + diffZ * diffZ;
+		float distance = Mathf.Sqrt (partDist);
+
+		AILookingScript looking = gameObject.GetComponent<AILookingScript>();
+
+		if (distance <= MinDist) {
+			looking.enabled = false;
+		} else {
+			looking.enabled = true;
+		}
+	}
+}
