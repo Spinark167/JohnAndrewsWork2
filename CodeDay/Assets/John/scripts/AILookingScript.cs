884547bc7cb871908fb7baaf6b3969de5aa44f7d
Spinark167 <jhandrews1@gmail.com>
2015-02-14 23:22:45 -0500

    Finished Grund!!!
    Look under my folders to find it if you need it.

---
+++ CodeDay/Assets/John/scripts/AILookingScript.cs
@@ -0,0 +1,110 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AILookingScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GameObject player = GameObject.Find ("Guy");
+
+		float playerX = player.transform.position.x;
+		float playerZ = player.transform.position.z;
+
+		float diffX = transform.position.x - playerX;
+		float diffZ = transform.position.z - playerZ;
+		//Debug.Log (diffX);
+		//Debug.Log (diffZ);
+
+		float TurnAround = 0;
+		//float TurnAroundZ = 0;
+
+		if (diffX >= 0) {
+			TurnAround = 180;
+		} else {
+			TurnAround = 0;			
+		}
+
+
+
+		float diff = diffZ / diffX;
+		//Debug.Log (diff);
+	
+		float AngDiff = Mathf.Atan (diff);
+		//Debug.Log (AngDiff);
+
+		float AngDiffDeg = ((AngDiff * 180) / Mathf.PI);
+
+		//float rotX = Mathf.Sin (AngDiff);
+		//float rotZ = Mathf.Cos (AngDiff);
+
+		transform.eulerAngles = new Vector3 (0f, -AngDiffDeg + TurnAround /*+ TurnAroundZ*/, 0f);
+
+		float rotOfObj = transform.rotation.eulerAngles.y;
+		//Debug.Log (rotOfObj);
+		float radRotObj = ((rotOfObj * Mathf.PI) / 180);
+
+		float factorX = Mathf.Sin (radRotObj);
+		float factorZ = Mathf.Cos (radRotObj);
+
+		//Debug.Log (factorX);
+		//Debug.Log (factorZ);
+
+		/*if (diffZ > 0) {
+			factorZ = factorZ * -1;
+		}*/
+
+		if (rotOfObj > 270 && rotOfObj < 360) {
+			factorX = factorX * -1;
+		}
+		if (rotOfObj > 0 && rotOfObj < 90) {
+			factorX = factorX * -1;
+		}
+		if (rotOfObj > 90 && rotOfObj < 180) {
+			factorX = factorX * -1;
+		}
+		if (rotOfObj > 180 && rotOfObj < 270) {
+			
+			factorX = factorX * -1;
+		}
+
+		if (rotOfObj < 2.5 || rotOfObj > 357.5) {
+			factorX = 0;
+			factorZ = 1;
+		}
+		if (rotOfObj < 92.5 && rotOfObj > 87.5) {
+			factorX = -1;
+			factorZ = 0;
+		}
+		if (rotOfObj < 182.5 && rotOfObj > 177.5) {
+			factorX = 0;
+			factorZ = -1;
+		}
+		if (rotOfObj < 272.5 && rotOfObj > 267.5) {
+			factorX = 1;
+			factorZ = 0;
+		}
+
+		rigidbody.velocity = new Vector3 (1f * factorZ, 0f, 1f * factorX);
+
+		if (rigidbody.velocity.x >= 2.5) {
+			rigidbody.velocity = new Vector3(2.4f ,rigidbody.velocity.y ,rigidbody.velocity.z);	
+		}
+		if (rigidbody.velocity.x <= -2.5) {
+			rigidbody.velocity = new Vector3(-2.4f ,rigidbody.velocity.y ,rigidbody.velocity.z);	
+		}
+		if (rigidbody.velocity.z >= 2.5) {
+			rigidbody.velocity = new Vector3(rigidbody.velocity.x ,rigidbody.velocity.y ,2.4f);	
+		}
+		if (rigidbody.velocity.z <= -2.5) {
+			rigidbody.velocity = new Vector3(rigidbody.velocity.x ,rigidbody.velocity.y ,-2.4f);	
+		}
+
+	}
+
+
+}
---
+++ CodeDay/Assets/John/scripts/AIShootScript.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AIShootScript : MonoBehaviour {
+
+	public GameObject thePrefab;
+	public AudioClip blipAudio;
+	
+	//public KeyCode shoot;
+	
+	public float Timer;
+	float TimeSet = .8f;
+
+	void Start () {
+		
+	}
+
+	void Update () {
+
+		float DegValX = ((transform.eulerAngles.y * Mathf.PI) / 180);
+		float factorX = Mathf.Sin (DegValX);
+		float factorZ = Mathf.Cos (DegValX);
+
+		GameObject cameraPivot = GameObject.Find ("GrundHolder");
+		float rotUp = cameraPivot.transform.rotation.eulerAngles.x;
+
+		float rotUpRad = ((rotUp * Mathf.PI) / 180);
+		float factorYUp = Mathf.Sin (rotUpRad);
+		float factorYOut = Mathf.Cos (rotUpRad);
+		
+		if (/*Input.GetKey (shoot) &&*/ Timer <= 0) {
+
+			Timer = TimeSet;
+			GameObject bullet = (GameObject)Instantiate (thePrefab, transform.position, /*Quaternion.Euler(Vector3(0, 0, 90))*/ transform.rotation);
+			
+			bullet.rigidbody.velocity = new Vector3(45f * factorX * factorYOut, -45f * factorYUp , 45f * factorZ * factorYOut);
+
+			AudioSource.PlayClipAtPoint(blipAudio, transform.position, 1f);
+		}
+	}
+	
+	void FixedUpdate(){
+		Timer -= .02f;
+	}
+}
